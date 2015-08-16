c24998eea2e9678c109d4f89941fca54336844ce
Alex Mejias <alexdmejias@gmail.com>
2015-08-16 19:00:26 -0400

    added actual code files

---
+++ js/modules/utils.js
@@ -0,0 +1,76 @@
+function Utils(cx, canvas) {
+  return {
+    cx : cx || '',
+    canvas: canvas || '',
+    halfX: function() {
+      return this.canvas.width / 2;
+    },
+    halfY: function() {
+      return this.canvas.height / 2;
+    },
+    range: function (min, max) {
+      if (!max) {
+        max = min;
+        min = 0;
+      }
+      return Math.floor(Math.random() * (max - min) + min);
+    },
+    range: function(min, max) {
+      var rand = Math.random();
+
+      if (arguments.length === 0) {
+        return rand;
+      } else
+      if (arguments.length === 1) {
+        return rand * min;
+      } else {
+        if (min > max) {
+          var tmp = min;
+          min = max;
+          max = tmp;
+        }
+
+        return rand * (max-min) + min;
+      }
+    },
+    // taken from the p5.js project
+    // https://github.com/processing/p5.js/blob/5c81d655f683f90452b80ab225a67e449463fff9/src/math/calculation.js#L394
+    map: function(n, start1, stop1, start2, stop2) {
+      return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
+    },
+
+    getMousePos: function(event) {
+      return {
+        x: event.clientX,
+        y: event.clientY
+      }
+    },
+
+    clear: function() {
+      this.cx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    W: canvas.width,
+    H: canvas.height,
+    HW: canvas.width / 2,
+    HH: canvas.height / 2,
+    ellipse: function(x, y, r) {
+      this.cx.beginPath();
+      this.cx.arc(x, y, r, 0, 2 * Math.PI, false);
+      this.cx.fill();
+      this.cx.stroke();
+    },
+    constrain: function(val, min, max) {
+      if (val > max) {
+        return max;
+      } else if (val < min) {
+        return min;
+      } else {
+        return val;
+      }
+    }
+  }
+};
+
+module.exports = function(cx, canvas) {
+  return new Utils(cx, canvas);
+};
